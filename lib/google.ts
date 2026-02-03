b36938efcf0ca5c9284f17f37c5a75a6b2614dfc
Leovaldi <leonelurielvaldiviaterrera@gmail.com>
2026-02-03 20:42:25 -0300

    Fix: actualizando configuracion de Google en lib/google.ts

--- lib/google.ts
+++ lib/google.ts
@@ -2,12 +2,10 @@ import { google } from 'googleapis';
 
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 
-const auth = new google.auth.JWT(
-  process.env.GOOGLE_CLIENT_EMAIL,
-  undefined,
-  process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  SCOPES
-);
+export const auth = new google.auth.JWT({
+  email: process.env.GOOGLE_CLIENT_EMAIL,
+  key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  scopes: SCOPES,
+});
 
 export const calendar = google.calendar({ version: 'v3', auth });
-export const CALENDAR_ID = process.env.GOOGLE_CALENDAR_ID;
